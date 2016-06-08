1f756e95ebeebb46715178eafebb902faa60275f
Mark Headd <mheadd@gmail.com>
2016-06-08 10:48:42 -0400

    remove call to second table for city/town name

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // URL & query strings for CartoDB API calls.
 var url_base = 'https://markh.cartodb.com/api/v2/sql?q=';
 var street_name_query = 'SELECT%20DISTINCT(streetname%20%7C%7C%20%27%20%27%20%7C%7C%20posttype)%20AS%20street%20FROM%20addresspoints_onondaga%20WHERE%20streetname%20LIKE%20%27[street-name]%25%27';
-var subdivision_name_query = 'SELECT%20citytownna%20FROM%20addresspoints_onondaga_citytown_name%20WHERE%20citytownna%20LIKE%20%27[sub-name]%25%27';
+var subdivision_name_query = 'SELECT%20DISTINCT(citytownna)%20FROM%20addresspoints_onondaga%20WHERE%20citytownna%20LIKE%20%27[sub-name]%25%27';
 
 // Autocomplete options for street name.
 var street_options = {
